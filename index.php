21792c63ed0929a2be8315465a25a2e0b4a50088
dustinmachat <dustinmachat@gmail.com>
2018-12-10 08:59:42 -0800

    Code moved and made efficient
    Moved error code to the top of page. Made code more efficient with else and &&

--- index.php
+++ index.php
@@ -4,14 +4,25 @@ include 'dbConfig.php';
 
 	//Logs In
 
-	// TODO: This can all be reduced to one if statement probably
 	if(isset($_SESSION["logged_in"])&&$_SESSION["logged_in"]==true&&(isset($_POST["username"])&&isset($_POST["password"])&&$_POST["username"]==$username&&$_POST["password"]==$password)){
 		$_SESSION["logged_in"]=true;header("Location success.php");
 	}
   //Checks if account deleted and redirected from Admin success.php
     if(isset($_POST["delete"])){
   mysqli_query($conn, "DELETE * FROM users WHERE username = " . $_POST["delete"]);
+
 }
+
+//Displays Errors
+
+if (isset($_GET["login"]) && $_GET["login"] == 0) {
+		echo "document.getElementById('error').innerHTML = 'Username or Password Incorrect';";
+} elseif (!empty($_GET["specialchar"])) {
+		echo "document.getElementById('error').innerHTML = 'Username or Password COntains Special Characters';";
+} elseif (!empty($new)) {
+		echo "document.getElementById('error').innerHTML = 'Login With Your New Account';";
+}
+
 ?>
 <html>
 <head>
@@ -29,25 +40,7 @@ Password:
 </form>
 
 <div id="red">
-<?php
-
-//Displays Errors
-// TODO: Move the php to the top of the page. Try to keep as much of the php
-// logic in one place. Set flags and variables use them to decide what to display.
-
-// TODO: Use && to make one if statement
-if (isset($_GET["login"])) {
-	if ($_GET["login"] == 0) {
-	echo "Username or Password Incorrect";
-    }
-}
-if (!empty($new)) {
-	echo "<br>Login With Your New Username and Password.<br>";
-}
-if (!empty($_GET["specialchar"])) {
-	echo "<br>Username and/or Password Contains Special Characters.<br>";
-}
-?>
+<p id="error"></p>
 </div>
 <br>
 Don't have an account?
